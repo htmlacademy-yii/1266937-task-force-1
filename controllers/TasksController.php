3fbe1a20791a0eca5d323cdeb12e7428ff560a45
angelina_n <angelinanosova@yandex.ru>
2026-04-12 02:30:27 +0300

    add controller for tasks page

---
+++ controllers/TasksController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use app\models\Task;
+
+class TasksController extends Controller
+{
+  public function actionIndex()
+  {
+    $tasks = Task::find()
+      ->where(['status' => 'new'])
+      ->orderBy(['created_at' => SORT_DESC])
+      ->all();
+
+    return $this->render(
+      'index',
+      [
+        'tasks' => $tasks
+      ]
+    );
+  }
+}
