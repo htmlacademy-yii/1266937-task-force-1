c697036eb1c34e584c445213e942acf65385cb1a
angelina_n <angelinanosova@yandex.ru>
2026-04-20 03:03:55 +0300

    use $_ENV for db config

--- web/index.php
+++ web/index.php
@@ -1,11 +1,17 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+Dotenv\Dotenv::createImmutable(__DIR__ . '/../')->load();
+
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
 
 $config = require __DIR__ . '/../config/web.php';
 
